cbb94ea5c9a62c30e45dc570dd5e018477de9d98
berkekirsallioba <berke.kirsallioba@gmail.com>
2021-01-16 23:43:46 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun.Adi +" Sepete Eklendi");
+
+        }
+
+        public void Ekle2(string UrunAdi, string aciklama, double fiyat,int adet)
+        {
+            Console.WriteLine(urun.Adi +" Sepete Eklendi");
+        }
+}
+}
